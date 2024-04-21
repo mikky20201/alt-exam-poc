e5357f4ef2098bf22e08688e739f57b91adf47ab
Micah Tobias <mikkywave@gmail.com>
2024-04-21 21:29:45 +0300

    AltSchool exam submission

--- infra_setup/init.sql
+++ infra_setup/init.sql
@@ -1,4 +1,3 @@
-
 -- Create schema
 CREATE SCHEMA IF NOT EXISTS ALT_SCHOOL;
 
@@ -6,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS ALT_SCHOOL;
 -- create and populate tables
 create table if not exists ALT_SCHOOL.PRODUCTS
 (
-    id  serial primary key,
+    id  bigint primary key,
     name varchar not null,
     price numeric(10, 2) not null
 );
@@ -27,19 +26,29 @@ FROM '/data/products.csv' DELIMITER ',' CSV HEADER;
 create table if not exists ALT_SCHOOL.ORDERS
 (
     order_id uuid not null primary key,
+    customer_id uuid not null,
+    status varchar not null,
+    checked_out_at timestamp not null
     -- provide the other fields
 );
 
+COPY ALT_SCHOOL.ORDERS (order_id, customer_id, status, checked_out_at)
+FROM '/data/orders.csv' DELIMITER ',' CSV HEADER;
 
 -- provide the command to copy orders data into POSTGRES
 
 
 create table if not exists ALT_SCHOOL.LINE_ITEMS
 (
-    line_item_id serial primary key,
+    line_item_id bigint primary key,
+    order_id uuid not null,
+    item_id bigint not null,
+    quantity bigint not null
     -- provide the remaining fields
 );
 
+COPY ALT_SCHOOL.LINE_ITEMS (line_item_id, order_id, item_id, quantity)
+FROM '/data/line_items.csv' DELIMITER ',' CSV HEADER;
 
 -- provide the command to copy ALT_SCHOOL.LINE_ITEMS data into POSTGRES
 
@@ -47,14 +56,22 @@ create table if not exists ALT_SCHOOL.LINE_ITEMS
 -- setup the events table following the examle provided
 create table if not exists ALT_SCHOOL.EVENTS
 (
+    event_id bigint primary key,
+    customer_id uuid not null,
+    event_data jsonb not null,
+    event_timestamp timestamp not null
     -- TODO: PROVIDE THE FIELDS
 );
-
+COPY ALT_SCHOOL.EVENTS (event_id,customer_id,event_data,event_timestamp)
+FROM '/data/events.csv' DELIMITER ',' CSV HEADER;
 -- TODO: provide the command to copy ALT_SCHOOL.EVENTS data into POSTGRES
-
-
-
-
-
-
-
+create table if not exists ALT_SCHOOL.CUSTOMERS
+(
+    customer_id uuid primary key,
+    device_id uuid not null,
+    location varchar not null,
+    currency varchar not null
+    -- TODO: PROVIDE THE FIELDS
+);
+COPY ALT_SCHOOL.CUSTOMERS (customer_id,device_id,location,currency)
+FROM '/data/customers.csv' DELIMITER ',' CSV HEADER;
